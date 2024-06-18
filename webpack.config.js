79f7f93ee4cd2e5d0be6193c156490787ffdaa18
ruasnms <maria.m.smirnova@raiffeisen.ru>
2024-06-18 17:12:50 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,8 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        historyApiFallback: true,
-        compress: true,
         port: 3000,
-        open: true,
-    },
+        contentBase: path.resolve(__dirname, "dist"),
+        historyApiFallback: { index: "/", disableDotRule: true },
+    }
 };
