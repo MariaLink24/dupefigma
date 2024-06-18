190abcc1f9af797dbf386ae2cca83ce5bbe01318
ruasnms <maria.m.smirnova@raiffeisen.ru>
2024-06-18 16:53:32 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <div/>,
+        },
+        {
+            path: '/auth',
+            element: <div/>,
+        },
+        {
+            path: '/select_room',
+            element: <div/>,
+        },
+        {
+            path: '/room',
+            element: <div/>,
+        },
+        {
+            path: '/create_room',
+            element: <div/>,
+        },
+        {
+            path: '/find_room',
+            element: <div/>,
+        },
+        {
+            path: '*',
+            element: <h1>404</h1>,
+        },
+    ]
+);
+
+
+
+ const App = () => {
+    return (<RouterProvider router={router}  />)
+}
+export default App;
